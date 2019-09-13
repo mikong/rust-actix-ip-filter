447a5270eaa5825a42e5258cddacf79faf1bda69
mikong <4162+mikong@users.noreply.github.com>
2019-09-13 16:41:05 +0800

    feat: Store IPs in AppState

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 
-fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use std::net::IpAddr;
+
+struct AppState {
+    ips: Vec<IpAddr>,
+}
+
+fn index(data: web::Data<AppState>) -> String {
+    match &data.ips.first() {
+        Some(&ip) => format!("First IP: {}", ip),
+        None => format!("No IPs found"),
+    }
 }
 
 fn main() {
     HttpServer::new(|| {
+        let ips = vec!["127.0.0.1".parse().unwrap()];
         App::new()
+            .data(AppState {
+                ips: ips
+            })
             .route("/", web::get().to(index))
     })
     .bind("127.0.0.1:8088")
