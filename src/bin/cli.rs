afab052d8e21c8250d29daa104a0816a6549d3ac
mikong <4162+mikong@users.noreply.github.com>
2019-09-13 23:58:23 +0800

    feat: Use command line REPL to add/remove IPs

---
+++ src/bin/cli.rs
@@ -0,0 +1,84 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+use std::io;
+use std::io::{Write};
+use std::net::IpAddr;
+
+#[path = "../models.rs"]
+mod models;
+#[path = "../schema.rs"]
+mod schema;
+
+use models::{NewIpAddress};
+
+fn main() {
+    let conn = establish_connection();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+
+        process_command(&command, &conn);
+    }
+}
+
+fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url).unwrap()
+}
+
+fn process_command(command: &str, conn: &MysqlConnection) {
+    let v: Vec<&str> = command.split_whitespace().collect();
+
+    if v.len() == 0 { return }
+
+    let cmd = v[0].to_ascii_uppercase();
+    if cmd == "ADD" {
+        add_ip(conn, v[1]);
+    } else if cmd == "REMOVE" {
+        remove_ip(conn, v[1]);
+    }
+    
+    if cmd == "EXIT" || cmd == "QUIT" {
+        std::process::exit(0);
+    }
+}
+
+fn add_ip(conn: &MysqlConnection, ip: &str) {
+    use schema::ip_addresses;
+
+    let ip_addr: IpAddr = ip.parse().expect("Not a valid IP address");
+
+    let new_ip_address = NewIpAddress {
+        ip: &ip_addr.to_string(),
+    };
+
+    diesel::insert_into(ip_addresses::table)
+        .values(&new_ip_address)
+        .execute(conn)
+        .expect("Error saving IP address");
+}
+
+fn remove_ip(conn: &MysqlConnection, ip_str: &str) -> String {
+    use schema::ip_addresses::dsl::*;
+
+    let ip_addr: IpAddr = ip_str.parse().expect("Not a valid IP address");
+
+    let num_deleted = diesel::delete(ip_addresses.filter(ip.eq(ip_addr.to_string())))
+        .execute(conn)
+        .expect("Error deleting IP addresses");
+
+    format!("Deleted {} IP addresses", num_deleted)
+}
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use diesel::prelude::*;
 use diesel::{r2d2::ConnectionManager, r2d2::Pool};
 use dotenv::dotenv;
 use std::env;
-use std::net::{IpAddr, SocketAddr};
+use std::net::{SocketAddr};
 
 mod actor;
 mod models;
@@ -17,7 +17,7 @@ mod schema;
 mod html_list;
 
 use actor::ws::WsActor;
-use models::{IpAddress, NewIpAddress};
+use models::{IpAddress};
 use html_list::HtmlList;
 
 struct AppState {
@@ -54,55 +54,12 @@ fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Erro
     Ok(HttpResponse::Forbidden().finish())
 }
 
-fn add(data: web::Data<AppState>) -> String {
-    let ip = "127.0.0.1";
-
-    let conn = &data.pool.get().unwrap();
-    add_ip(conn, ip);
-
-    "OK".to_string()
-}
-
-fn remove(data: web::Data<AppState>) -> String {
-    let ip = "127.0.0.1";
-
-    let conn = &data.pool.get().unwrap();
-    remove_ip(conn, ip)
-}
-
-fn add_ip(conn: &MysqlConnection, ip: &str) {
-    use schema::ip_addresses;
-
-    let ip_addr: IpAddr = ip.parse().expect("Not a valid IP address");
-
-    let new_ip_address = NewIpAddress {
-        ip: &ip_addr.to_string(),
-    };
-
-    diesel::insert_into(ip_addresses::table)
-        .values(&new_ip_address)
-        .execute(conn)
-        .expect("Error saving IP address");
-}
-
 fn ip_exists(conn: &MysqlConnection, ip_str: &str) -> bool {
     use schema::ip_addresses::dsl::*;
 
     diesel::select(diesel::dsl::exists(ip_addresses.filter(ip.eq(ip_str))))
         .get_result(conn)
         .expect("Error checking existence")
-}
-
-fn remove_ip(conn: &MysqlConnection, ip_str: &str) -> String {
-    use schema::ip_addresses::dsl::*;
-
-    let ip_addr: IpAddr = ip_str.parse().expect("Not a valid IP address");
-
-    let num_deleted = diesel::delete(ip_addresses.filter(ip.eq(ip_addr.to_string())))
-        .execute(conn)
-        .expect("Error deleting IP addresses");
-
-    format!("Deleted {} IP addresses", num_deleted)
 }
 
 fn establish_connection() -> Pool<ConnectionManager<MysqlConnection>> {
@@ -132,9 +89,7 @@ fn main() {
                     .data(AppState {
                         pool: pool.clone(),
                     })
-                    .route("/ips", web::get().to(index))
-                    .route("/add", web::get().to(add))
-                    .route("/remove", web::get().to(remove)))
+                    .route("/ips", web::get().to(index)))
     })
     .bind("127.0.0.1:8088")
     .unwrap()
